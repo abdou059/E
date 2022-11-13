6c6545eb3abb75d3a1a5794948399b14d12a7376
abdou059 <abdoumonuir9@gmail.com>
2022-11-13 20:45:21 +0200

    filel

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import styled from 'styled-components';
+
+const content = styled.div({
+  color:'red',
+});
+
+export default function Navbar() {
+  return (
+          
+    <content>
+      Navbar
+    </content>
+  )
+}
